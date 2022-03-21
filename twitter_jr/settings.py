6b851f51d4296980e72fa6820138a6fdd7d6c570
mbarricklow <meghan.barricklow@gmail.com>
2022-03-21 07:35:49 -0400

    Initial Commit

--- twitter_jr/settings.py
+++ twitter_jr/settings.py
@@ -9,11 +9,10 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-import os # new
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# Remember on same level as manage.py
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -21,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#+b6$48ua$86leatzsn+2$_-(4x(qeh54laxehbgjp2#mjbtd='
+SECRET_KEY = 'django-insecure-n@2t#0_8=^5xn*vxgcszi0l9#fx%*v%2_8vosxxlcmz9gp-vt&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -35,9 +34,9 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tweets',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +54,7 @@ ROOT_URLCONF = 'twitter_jr.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "templates")],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- twitter_jr/tweets/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Tweet(models.Model):
-    # Maps to MySQL data
-    # id = models.AutoField(primary_key=True)
-    content = models.TextField()
-    image = models.FileField(upload_to='images/', blank=True, null=True)
