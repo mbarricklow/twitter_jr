7fe3f76fb5b2810922f728516de17266d558725d
mbarricklow <meghan.barricklow@gmail.com>
2022-03-21 17:34:58 -0400

    25 - JavaScript updates

---
+++ tweets/views.py
@@ -0,0 +1,37 @@
+from django.http import HttpResponse, Http404, JsonResponse
+from django.shortcuts import render
+
+from .models import Tweet
+
+# Create your views here.
+def home_view(request, *args, **kwargs):
+    # return HttpResponse("<h1>Hello World</h1>")
+    return render(request, "pages_\home.html", context={}, status=200)
+
+def tweet_list_view(request, *args, **kwargs):
+    qs = Tweet.objects.all()
+    tweets_list = [{"id": x.id, "content": x.content} for x in qs]
+    data = {
+        "isUser": False,
+        "response": tweets_list
+    }
+    return JsonResponse(data)
+
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    """
+    REST API VIEW
+    Consume by JavaScript or Swift/Java/i0S/Android
+    return json data
+    """
+    data = {
+        "id": tweet_id,
+        # "image_path": obj.image.url
+    }
+    status = 200
+    try:
+        obj = Tweet.objects.get(id=tweet_id)
+        data['content'] = obj.content
+    except:
+        data['message'] = "Not found"
+        status = 404
+    return JsonResponse(data, status=status)
