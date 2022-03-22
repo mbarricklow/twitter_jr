ead496d459029060915a2e9abe53762499af31cd
mbarricklow <meghan.barricklow@gmail.com>
2022-03-22 07:58:17 -0400

    Added "likes" buttons and navbar.html

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,4 @@
+import random
 from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
 
@@ -10,7 +11,7 @@ def home_view(request, *args, **kwargs):
 
 def tweet_list_view(request, *args, **kwargs):
     qs = Tweet.objects.all()
-    tweets_list = [{"id": x.id, "content": x.content} for x in qs]
+    tweets_list = [{"id": x.id, "content": x.content, "likes": random.randint(0, 300)} for x in qs]
     data = {
         "isUser": False,
         "response": tweets_list
